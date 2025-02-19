4c640b8fde5e71014fea741ffb79060a5093c3cc
Hamsikakrishnan <r.hamsikakrishnan@gmail.com>
2025-02-19 17:19:58 +0530

    Basic Things Done

---
+++ app/(components)/Nav.js
@@ -0,0 +1,5 @@
+export default function Nav() {
+    return (
+        <h1>This is Navbar</h1>
+    )
+}
---
+++ app/TicketPage/[id]/page.js
@@ -0,0 +1,7 @@
+export default function TicketPage({params}) {
+  return (
+    <div>
+      <h1>TicketPage {params.id}</h1>
+    </div>
+  );
+}
