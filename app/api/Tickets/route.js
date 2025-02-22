6f536fccd2ef4a105af3eb7ade605aea0806990f
Hamsikakrishnan <r.hamsikakrishnan@gmail.com>
2025-02-22 18:40:47 +0530

    CRUD Operations

--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -1,9 +1,16 @@
 import Ticket from "../../models/Ticket";
 import { NextResponse } from "next/server";
-
+export async function GET(req){
+    try{
+        const tickets = await Ticket.find({});
+        return NextResponse.json(tickets, { status: 200 });
+    }
+    catch(error){
+        return NextResponse.json({ message: "Error", error }, { status: 500 });
+    }
+}
 export async function POST(req){
     try{
-        
         const body = await req.json();
         const newTicket = body.formData;
         await Ticket.create(newTicket);
